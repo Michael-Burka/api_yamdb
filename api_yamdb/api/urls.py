d5da3a4c169ab86b346385ae49ee6d3a48be50a5
Алексей Ларионов <Alakey060711@yandex.ru>
2023-09-02 16:52:19 +0500

    Wrong merge.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,26 +1,9 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from .views import TitleViewSet, GenreViewSet, CategoryViewSet
-
-router = DefaultRouter()
-router.register('categories', CategoryViewSet, basename='categories')
-router.register('genres', GenreViewSet, basename='genres')
-router.register('titles', TitleViewSet, basename='titles')
-
-urlpatterns = [path('v1/', include(router.urls))]
-
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
 from api.views import (
-  EmailActivation,
-  UserViewSet,
-  SignUp,
-  TitleViewSet,
-  GenreViewSet,
-  CategoryViewSet
-)
+    EmailActivation, UserViewSet, SignUp,
+    TitleViewSet, GenreViewSet, CategoryViewSet)
 
 app_name = 'api'
 
@@ -39,4 +22,3 @@ urlpatterns = [
     # Эндпоинт для активации и получения токена
     path('v1/auth/token/', EmailActivation.as_view(), name='activation'),
 ]
-
