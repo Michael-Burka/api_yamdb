c5ee12a73470bcb3ebf3de4ff9bbec81584d9999
Алексей Ларионов <Alakey060711@yandex.ru>
2023-09-02 15:49:04 +0500

    Add Review model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-from .validators import validate_slug, valildate_year
+from users.models import User
+from reviews.validators import validate_slug, valildate_year
 
 SLUG_MAX_LENGTH = 50
 NAME_MAX_LENGTH = 256
@@ -8,17 +10,17 @@ NAME_MAX_LENGTH = 256
 
 class Category(models.Model):
     name = models.CharField(
-            max_length=NAME_MAX_LENGTH,
-            verbose_name='Название категории',
-            help_text='Введите название категории',
-            )
+        max_length=NAME_MAX_LENGTH,
+        verbose_name='Название категории',
+        help_text='Введите название категории',
+    )
     slug = models.SlugField(
-            max_length=SLUG_MAX_LENGTH,
-            verbose_name='Слаг категории',
-            unique=True,
-            help_text='Введите слаг категории',
-            validators=[validate_slug],
-            )
+        max_length=SLUG_MAX_LENGTH,
+        verbose_name='Слаг категории',
+        unique=True,
+        help_text='Введите слаг категории',
+        validators=[validate_slug],
+    )
 
     def __str__(self) -> str:
         return self.name
@@ -30,17 +32,17 @@ class Category(models.Model):
 
 class Genre(models.Model):
     name = models.CharField(
-            max_length=NAME_MAX_LENGTH,
-            verbose_name='Название жанра',
-            help_text='Введите название жанра',
-            )
+        max_length=NAME_MAX_LENGTH,
+        verbose_name='Название жанра',
+        help_text='Введите название жанра',
+    )
     slug = models.SlugField(
-            max_length=SLUG_MAX_LENGTH,
-            verbose_name='Слаг жанра',
-            unique=True,
-            help_text='Введите слаг жанра',
-            validators=[validate_slug],
-            )
+        max_length=SLUG_MAX_LENGTH,
+        verbose_name='Слаг жанра',
+        unique=True,
+        help_text='Введите слаг жанра',
+        validators=[validate_slug],
+    )
 
     def __str__(self) -> str:
         return self.name
@@ -52,37 +54,37 @@ class Genre(models.Model):
 
 class Title(models.Model):
     name = models.CharField(
-            max_length=NAME_MAX_LENGTH,
-            verbose_name='Название произведения',
-            help_text='Введите название произведения',
-            db_index=True,
-            )
+        max_length=NAME_MAX_LENGTH,
+        verbose_name='Название произведения',
+        help_text='Введите название произведения',
+        db_index=True,
+    )
     year = models.PositiveSmallIntegerField(
-            verbose_name='Год выпуска',
-            help_text='Введите год выпуска произведения',
-            db_index=True,
-            validators=[valildate_year],
-            )
+        verbose_name='Год выпуска',
+        help_text='Введите год выпуска произведения',
+        db_index=True,
+        validators=[valildate_year],
+    )
     description = models.TextField(
-            verbose_name='Описание произведения',
-            help_text='Введите описание произведения',
-            blank=True,
-            null=True,
-            )
+        verbose_name='Описание произведения',
+        help_text='Введите описание произведения',
+        blank=True,
+        null=True,
+    )
     category = models.ForeignKey(
-            Category,
-            null=True,
-            on_delete=models.SET_NULL,
-            verbose_name='Категория',
-            help_text='Выберите категорию произведения',
-            related_name='titles',
-            )
+        Category,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория',
+        help_text='Выберите категорию произведения',
+        related_name='titles',
+    )
     genre = models.ManyToManyField(
-            Genre,
-            verbose_name='Жанр',
-            help_text='Выберите жанр произведения',
-            )
-    
+        Genre,
+        verbose_name='Жанр',
+        help_text='Выберите жанр произведения',
+    )
+
     def __str__(self) -> str:
         return self.name
 
@@ -92,8 +94,38 @@ class Title(models.Model):
 
 
 class Review(models.Model):
-    pass
+    text = models.TextField('Текст отзыва')
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        db_index=True
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        db_index=True
+    )
+    score = models.PositiveSmallIntegerField(
+        verbose_name='Оценка',
+        validators=(MinValueValidator(1), MaxValueValidator(10))
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    def __str__(self):
+        return self.text[:10]
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
 
 
 class Comment(models.Model):
-    pass 
+    pass
